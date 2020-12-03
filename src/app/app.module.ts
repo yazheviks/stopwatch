6fb443caafa344a5ad498a45d6834d30a9e0ea59
Yazheviks <vicsergeyeva@gmail.com>
2020-12-03 15:58:41 +0200

    stopwatch implements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StopwatchComponent } from './stopwatch/stopwatch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StopwatchComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import { timer, fromEvent, Subscription } from 'rxjs';
+import { timeInterval } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-stopwatch',
+  templateUrl: './stopwatch.component.html',
+  styleUrls: ['./stopwatch.component.scss']
+})
+export class StopwatchComponent  {
+  isStarted:boolean = false;
+  initialState = {
+    time: 0,
+    hour: 0,
+    minute: 0,
+    second: 0,
+  };
+  state = { ...this.initialState };
+  doubleClickDifference = 300;
+  a: Subscription = new Subscription;
+
+  renderNewTime(time: number) {
+    switch(this.isStarted) {
+      case time < 60:
+        this.state.second = this.state.time;
+        break;
+      case time < 3600:
+        this.state.minute = Math.floor(time / 60);
+        this.state.second = Math.floor(time % 60);
+        break;
+      default:
+        this.state.hour = Math.floor(time / 3600);
+        this.state.minute = Math.floor(time % 3600 / 60);
+        this.state.second = Math.floor(time % 3600 % 60);
+    }
+  }
+
+  start(): void {
+    this.a = timer(1000, 1000).subscribe(() => {
+      this.state.time++;
+
+      this.renderNewTime(this.state.time);
+    });
+  }
+
+  finish(): void {
+    this.a.unsubscribe();
+    this.reset();
+  }
+
+  toggleWork(): void {
+    this.isStarted = !this.isStarted;
+
+    if (!this.isStarted) {
+      this.finish();
+      return;
+    }
+
+    this.start();
+  }
+
+  wait(event: any): void {
+    if (!this.isStarted) {
+      return;
+    }
+
+    fromEvent(event.target, 'click')
+      .pipe(timeInterval())
+      .subscribe(iteration => {
+        if (iteration.interval < this.doubleClickDifference) {
+          this.isStarted = false;
+          this.a.unsubscribe();
+        }
+    });
+  }
+
+  reset(): void {
+    if (!this.state.time) {
+      return;
+    }
+
+    this.state = { ...this.initialState };
+  }
+}
